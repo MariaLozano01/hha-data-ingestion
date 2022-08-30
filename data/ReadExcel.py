d647055246f577ecadddd2265cd1e3bedb7bc135
MariaLozano01 <102619474+MariaLozano01@users.noreply.github.com>
2022-08-30 17:51:14 -0400

    Update

--- data/ReadExcel.py
+++ data/ReadExcel.py
@@ -11,15 +11,26 @@ tab1
 tab2 
 import requests #call requests package
 apidataset = 'https://data.cms.gov/data-api/v1/dataset/332846e4-ea55-4641-8307-59ea4e59a5a0/data'
-data = apidataset
-data = requests.get(apidataset)
-data = data.json()
+data = apidataset #Defining variable data
+data = requests.get(apidataset) #bring in open source json api and call it apidataset
+data = data.json() #runs requests as json
 
 ## Section 3
 from google.cloud import bigquery # import bigquery
 GOOGLE_APPLICATION_CREDENTIALS = 'hha-data-ingestion-361019-30da82e962c4.json'
 client = bigquery.Client.from_service_account_json('/Users/marialozano/Downloads/hha-data-ingestion HW FILES/hha-data-ingestion-361019-30da82e962c4.json')
+#querying public dataset1
+query_job = client.query('SELECT * FROM `patents-public-data.google_patents_research.publications_201710` LIMIT 100')
+## get results
+results = query_job.result()
+## put results into dataframe for dataset1
+bigquery1 = pd.DataFrame(results.to_dataframe()) ## put results into dataframe
 
-
-
-
+## dataset2
+client = bigquery.Client.from_service_account_json('/Users/marialozano/Downloads/hha-data-ingestion HW FILES/hha-data-ingestion-361019-30da82e962c4.json')
+#querying public dataset2
+query_job = client.query('SELECT * FROM `patents-public-data.google_patents_research.publications_201809` LIMIT 100')
+## get results
+results = query_job.result()
+## put results into dataframe for dataset2 
+bigquery2 = pd.DataFrame(results.to_dataframe()) ## put results into dataframe
